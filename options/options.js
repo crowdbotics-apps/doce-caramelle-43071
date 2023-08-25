983da371ced269c1c793bc233b0795226b8ae885
Crowdbotics <team@crowdbotics.com>
2023-08-25 17:27:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "doce-caramelle-43071",
+  url: "https://doce-caramelle-43071.botics.co",
+  api: "https://doce-caramelle-43071.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
